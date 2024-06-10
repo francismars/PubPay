0a4dec872076d2bb849ce320048af66935c6012a
francismars <francisconlm@hotmail.com>
2024-06-10 19:46:40 +0100

    testing amber integration

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -119,7 +119,7 @@ async function createZapEvent(eventStoragePK, pubKey = null){
 
 document.addEventListener("visibilitychange", async function() {
   if (document.visibilityState === 'visible') {
-    let eventStoragePK = sessionStorage.setItem('AmberPubkey', 'true');
+    let eventStoragePK = JSON.parse(sessionStorage.getItem("AmberPubkey"));
     console.log(eventStoragePK)
     if(eventStoragePK){
       sessionStorage.removeItem('AmberPubkey');
