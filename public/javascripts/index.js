63e5640860acc3aa08cb70ab49da87ebe8bfa05e
francismars <francisconlm@hotmail.com>
2024-06-10 14:30:33 +0100

    testing amber integration

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -115,7 +115,7 @@ document.addEventListener("visibilitychange", onVisibilityChange);
 async function onVisibilityChange() {
   if (document.visibilityState === "visible") {
     const eventStorage = sessionStorage.getItem("AmberSign");
-    console.log("Object:", eventStorage.callback)
+    console.log("Object:", eventStorage)
     if(eventStorage!=null){
       const eventSigned = await navigator.clipboard.readText();
       zapFinalized = finalizeEvent(eventStorage.event, eventSigned)
