ab539eed4af0589a57e8e59e74c4d4060eeaaa4d
francismars <11526661+francismars@users.noreply.github.com>
2025-05-07 00:43:46 +0100

    debugging mobile

--- public/javascripts/util.js
+++ public/javascripts/util.js
@@ -13,10 +13,11 @@ export function showJSON(json) {
 }
 
 export async function accessClipboard() {
+  alert("Please allow clipboard access in your browser settings.");
   return new Promise((resolve) => {
     setTimeout(async () => {
       let clipcopied = await navigator.clipboard.readText();
-      //console.log(clipcopied)
+      alert(clipcopied);
       resolve(clipcopied);
     }, 500);
   });
