c8410ddb9519a62c16ab9cab38d085592c5116c4
francismars <francisconlm@hotmail.com>
2025-09-03 14:20:04 +0100

    Refactor live.js to prioritize note ID from URL path, enhance loading logic, and streamline URL updates. Update live.html and style.css for consistent resource paths and remove invalid CSS selectors.

--- routes/live.js
+++ routes/live.js
@@ -3,9 +3,12 @@ var router = express.Router();
 const path = require('path');
 
 /* GET home page. */
-
-
 router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/../views/live.html'));
+});
+
+/* GET live page with note ID */
+router.get('/:noteId', function(req, res, next) {
   res.sendFile(path.join(__dirname+'/../views/live.html'));
 });
 
